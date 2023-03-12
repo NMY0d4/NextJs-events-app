15998f2699e4462be7b22ea3d2abdcddab0f77e3
Grégory Marini <greg.marini@hotmail.fr>
2023-03-12 17:54:42 +0100

    add content and style for event

---
+++ components/events/event-item.js
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import React from 'react';
+import styles from './event-item.module.scss';
+
+export default function EventItem({ event }) {
+  const { title, image, date, location, id } = event;
+  const humanReadableDate = new Date(date).toLocaleDateString('fr-FR', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+  const formattedAddress = location.replace(', ', '\n');
+  const exploreLink = `/events/${id}`;
+  return (
+    <li className={styles.item}>
+      <img src={`/${image}`} alt='' />
+      <div className={styles.content}>
+        <div className={styles.summary}>
+          <h2>{title}</h2>
+          <div className={styles.date}>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={styles.address}>
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+        <div className={styles.actions}>
+          <Link href={exploreLink}>Explore Event</Link>
+        </div>
+      </div>
+    </li>
+  );
+}
