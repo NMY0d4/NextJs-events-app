e7c3e8584f47352db446fcf8a3875f8aa8e4ea11
Grégory Marini <greg.marini@hotmail.fr>
2023-03-13 18:45:46 +0100

    add event detail page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,25 @@
+import { Router, useRouter } from 'next/router';
+import { Fragment } from 'react';
+import EventLogistics from '../../components/events/event-logistics';
+import EventSummary from '../../components/events/event-summary';
+import EventContent from '../../components/events/event-content';
+import { getEventById } from '../../dummy-data';
+
 export default function Event() {
+  const router = useRouter();
+  const event = getEventById(router.query.eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
   return (
-    <div>
-      <h1>One event.</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title}/>      
+      <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title}/>
+      <EventContent>
+        {event.description}
+      </EventContent>
+    </Fragment>
   );
 }
