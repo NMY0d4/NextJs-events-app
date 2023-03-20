0945697b39aeb3e8c6e9a91273a41e170c3db687
Grégory <greg.marini@hotmail.fr>
2023-03-20 12:05:37 +0100

    optimize image with Image from nextJS

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -4,6 +4,7 @@ import styles from './event-item.module.scss';
 import DateIcon from '../icons/date-icon';
 import AddressIcon from '../icons/address-icon';
 import ArrowRightIcon from '../icons/arrow-right-icon';
+import Image from 'next/image';
 
 export default function EventItem({ event }) {
   const { title, image, date, location, id } = event;
@@ -17,7 +18,7 @@ export default function EventItem({ event }) {
   const exploreLink = `/events/${id}`;
   return (
     <li className={styles.item}>
-      <img src={`/${image}`} alt='' />
+      <Image src={`/${image}`} alt={title} width={250} height={160} />
       <div className={styles.content}>
         <div className={styles.summary}>
           <h2>{title}</h2>
