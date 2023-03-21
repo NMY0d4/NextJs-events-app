d05dca988d3dc39f97e2b4c900d79499329c8c99
Grégory Marini <greg.marini@hotmail.fr>
2023-03-21 19:48:50 +0100

    add api route for newsletter

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,12 +1,22 @@
+import { useRef } from 'react';
 import classes from './newsletter-registration.module.css';
 
 function NewsletterRegistration() {
-  function registrationHandler(event) {
-    event.preventDefault();
+  const emailRef = useRef();
 
-    // fetch user input (state or refs)
-    // optional: validate input
-    // send valid data to API
+  function registrationHandler(e) {
+    e.preventDefault();
+    const emailValue = emailRef.current.value;
+
+    fetch('/api/newsletter', {
+      method: 'POST',
+      body: JSON.stringify({ email: emailValue }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
   }
 
   return (
@@ -15,6 +25,7 @@ function NewsletterRegistration() {
       <form onSubmit={registrationHandler}>
         <div className={classes.control}>
           <input
+            ref={emailRef}
             type='email'
             id='email'
             placeholder='Your email'
