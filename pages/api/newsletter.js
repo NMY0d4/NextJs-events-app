b69df92901d2a3934c1d585c18d62b5d389e4ef2
Grégory Marini <greg.marini@hotmail.fr>
2023-03-23 13:55:52 +0100

    add mongodb and use it for newsletter

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,8 @@
-export default function handler(req, res) {
+import { MongoClient } from 'mongodb';
+require('dotenv').config();
+import { mongoConnect } from '../../services/mongo';
+
+export default async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
 
@@ -7,7 +11,12 @@ export default function handler(req, res) {
       return;
     }
 
-    console.log(userEmail);
+    const client = await MongoClient.connect(process.env.MONGO_DB_URL);
+    const db = client.db();
+    await db.collection('emails').insertOne({ email: userEmail });
+
+    client.close();
+
     res.status(201).json({ message: 'Signed up!' });
   }
 }
