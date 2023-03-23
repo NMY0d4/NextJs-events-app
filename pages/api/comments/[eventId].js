fc251d49679e4ed861e8996e987c257e8fc06043
Grégory Marini <greg.marini@hotmail.fr>
2023-03-23 10:35:26 +0100

    create comment route in api for create and read comments in front

---
+++ pages/api/comments/[eventId].js
@@ -0,0 +1,49 @@
+export default function handler(req, res) {
+  const { eventId } = req.query;
+  if (req.method === 'POST') {
+    // add server side validation
+    const { email, name, text } = req.body;
+
+    if (
+      !email.includes('@') ||
+      !name ||
+      name.trim() === '' ||
+      !text ||
+      text.trim() === ''
+    ) {
+      res.status(422).json({ message: 'Invalid input.' });
+      return;
+    }
+
+    console.log(email, name, text);
+    const newComment = {
+      id: new Date().toISOString(),
+      email,
+      name,
+      text,
+    };
+
+    console.log(newComment);
+
+    res.status(201).json({ message: 'Added comment.', comment: newComment });
+  }
+
+  if (req.method === 'GET') {
+    const dummyList = [
+      {
+        id: 'c1',
+        name: 'Grégory',
+        email: 'test1@test.com',
+        comment: 'first comment test',
+      },
+      {
+        id: 'c2',
+        name: 'Sarah',
+        email: 'test2@test.com',
+        comment: 'blabla bla bla et blabla',
+      },
+    ];
+
+    res.status(200).json({ comments: dummyList });
+  }
+}
