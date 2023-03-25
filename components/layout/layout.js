a0c7143b522eb701c08a1f6fe2070893873e6e94
Grégory Marini <greg.marini@hotmail.fr>
2023-03-25 11:50:49 +0100

    add notification display in layout component

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,12 +1,22 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
 import MainHeader from './main-header';
+import Notification from '../ui/notification';
+import NotificationContext from '../../store/notification-context';
 
 export default function Layout({ children }) {
-  
+  const notificationCtx = useContext(NotificationContext);
+
+  const activeNotification = notificationCtx.notification;
+
+  const { title, message, status } = activeNotification;
+
   return (
     <Fragment>
       <MainHeader />
       <main>{children}</main>
+      {activeNotification && (
+        <Notification title={title} message={message} status={status} />
+      )}
     </Fragment>
   );
 }
